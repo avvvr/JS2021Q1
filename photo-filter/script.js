b54df74aa4856ebe1eb9042ebd5f60c354581904
Alina Balysh <lina.balysh@gmail.com>
2021-04-26 14:01:25 +0300

    feat: add changing images after clicking next btn

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -74,5 +74,44 @@ resetBtn.addEventListener("click", () => {
     }
   }
 
-  canvas.style.filter="blur(0px) invert(0%) sepia(0%) saturate(100%) hue-rotate(0deg)";
+  canvas.style.filter = "blur(0px) invert(0%) sepia(0%) saturate(100%) hue-rotate(0deg)";
+})
+
+let nextBtn = document.querySelector(".btn-next");
+let imgNumber = 1;
+let imgNumberStr = "01";
+let timesOfDay = ""; //время суток
+
+nextBtn.addEventListener("click", () => {
+  let currentDate = new Date();
+  let hour = currentDate.getHours();
+
+  if (imgNumber < 10) {
+    imgNumberStr = `0${imgNumber}`;
+  } else {
+    imgNumberStr = `${imgNumber}`;
+  }
+
+  if (hour >= 6 && hour <= 11) {
+    timesOfDay = "morning"
+  } else if (hour >= 12 && hour <= 17) {
+    timesOfDay = "day"
+  } else if (hour >= 18 && hour <= 23) {
+    timesOfDay = "evening"
+  } else if (hour >= 0 && hour <= 5) {
+    timesOfDay = "night "
+  }
+
+  img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timesOfDay}/${imgNumberStr}.jpg`;
+  console.log(img.src);
+  console.log(`imgNumber: ${imgNumber}`);
+
+  img.onload = function () {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    context.drawImage(img, 0, 0);
+  };
+
+  if (imgNumber === 20) imgNumber = 1;
+  else imgNumber++;
 })
